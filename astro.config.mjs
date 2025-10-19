9f7f536e504029e63ed0d772262ff687fae51d2f
yob <yersonolivares@protonmail.com>
2025-10-18 23:02:16 -0300

    Add Astro Node adapter and start script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,7 @@ export default defineConfig({
       allowedHosts: ["home.yaob.cl"],
     },
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
